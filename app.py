ef7da663d29be4de717c082e69b0a8c1f4bde066
TimWoo <Tim.Woods@softwire.com>
2020-05-29 11:37:51 +0100

    Add new todo functionality

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ import session_items as session
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     todos = session.get_items()
     return render_template('index.html', todos=todos)
+
+
+@app.route('/new', methods=['POST'])
+def newtodo():
+    title = request.form.get('title')
+    session.add_item(title)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
