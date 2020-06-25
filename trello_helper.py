bc30269f07163099ddfcdc020eca5ea962d4936a
TimWoo <Tim.Woods@softwire.com>
2020-06-25 14:04:05 +0100

    Add get_items method, which reads from trello

--- trello_helper.py
+++ trello_helper.py
@@ -1,2 +1,41 @@
 import requests
+import secrets
+
+_DEFAULT_ITEMS = [
+    { 'id': 1, 'status': 'Not Started', 'title': 'List saved todo items' },
+    { 'id': 2, 'status': 'Not Started', 'title': 'Allow new items to be added' }
+]
+
+_BASE_API_URL = "https://api.trello.com/1/"
+_DEFAULT_PAYLOAD = {'key': secrets.trello_key , 'token': secrets.trello_token}
+_BOARD_ID = "5ef48df22d51c825656d5f8c"
+_TODO_LIST_ID = "5ef48eae04ceb72b492830ef"
+_DONE_LIST_ID = "5ef48eb03da6260d87a4e9ab"
+
+def get_items():
+    """
+    Fetches all saved items from the session.
+
+    Returns:
+        list: The list of saved items.
+    """
+    r = requests.get(_BASE_API_URL + "boards/" + _BOARD_ID + "/cards", params=_DEFAULT_PAYLOAD)
+    return parse_cards(r)
+
+def parse_cards(raw_request):
+    cards_as_json = raw_request.json()
+    card_list = []
+    for card in cards_as_json:
+        card_list.append(parse_card(card))
+
+    return card_list
+
+def parse_card(card):
+    return {'id': card['id'], 'status': list_id_to_list_friendly_name(card['idList']), 'title': card['name']}
+
+def list_id_to_list_friendly_name(list_id):
+    if list_id == _TODO_LIST_ID:
+        return "Not Started"
+    if list_id == _DONE_LIST_ID:
+        return "Complete"
 
