fe2031a0dd19fc1b123ab6f4b8185122d2deb705
TimWoo <Tim.Woods@softwire.com>
2020-07-17 11:21:58 +0100

    Add view_model class and set app and template to use it

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for
 from trello_helper import TrelloAPI as trello
+from view_model import ViewModel
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
     todos = trello.get_items()
-    return render_template('index.html', todos=todos)
+    item_view_model = ViewModel(todos)
+    # return render_template('index.html', todos=todos)
+    return render_template('index.html', view_model = item_view_model)
 
 @app.route('/new', methods=['POST'])
 def new_todo():
