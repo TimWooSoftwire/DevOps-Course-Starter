1efe7ffe080fae09b69a84ee3bb5890edd8d8f72
TimWoo <Tim.Woods@softwire.com>
2020-07-24 08:45:01 +0100

    refactoring in preparation for adding integration tests

--- app.py
+++ app.py
@@ -2,35 +2,37 @@ from flask import Flask, render_template, request, redirect, url_for
 from trello_helper import TrelloAPI as trello
 from view_model import ViewModel
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    
+    @app.route('/', methods=['GET'])
+    def index():
+        todos = trello.get_items()
+        item_view_model = ViewModel(todos)
+        return render_template('index.html', view_model = item_view_model)
 
-@app.route('/', methods=['GET'])
-def index():
-    todos = trello.get_items()
-    item_view_model = ViewModel(todos)
-    # return render_template('index.html', todos=todos)
-    return render_template('index.html', view_model = item_view_model)
+    @app.route('/new', methods=['POST'])
+    def new_todo():
+        title = request.form.get('title')
+        trello.add_item(title)
+        return redirect('/')
 
-@app.route('/new', methods=['POST'])
-def new_todo():
-    title = request.form.get('title')
-    trello.add_item(title)
-    return redirect('/')
+    @app.route('/done/<id>', methods=['POST'])
+    def mark_as_complete(id):
+        trello.Move_item_to_done(id)
+        return redirect('/')
 
-@app.route('/done/<id>', methods=['POST'])
-def mark_as_complete(id):
-    trello.Move_item_to_done(id)
-    return redirect('/')
+    @app.route('/doing/<id>', methods=['POST'])
+    def mark_as_doing(id):
+        trello.move_item_to_doing(id)
+        return redirect('/')
 
-@app.route('/doing/<id>', methods=['POST'])
-def mark_as_doing(id):
-    trello.move_item_to_doing(id)
-    return redirect('/')
+    @app.route('/delete/<id>', methods=['POST'])
+    def delete(id):
+        trello.delete_item(id)
+        return redirect('/')
 
-@app.route('/delete/<id>', methods=['POST'])
-def delete(id):
-    trello.delete_item(id)
-    return redirect('/')
+    return app
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
